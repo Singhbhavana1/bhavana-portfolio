dd1a6a90acdbef2bb86f2b062bf81243f7a15682
bhavanasingh01 <bhavana0105singh@gmail.com>
2024-11-08 02:41:52 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrap } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full ">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.heroSubText}>Introduction</p>
+        <h2 className={styles.heroHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I’m a passionate frontend developer with a focus on creating intuitive
+        and engaging user experiences .As a frontend developer with expertise in
+        Angular, React, Node.js, and other technologies, I’m dedicated to
+        crafting seamless, engaging user experiences. Currently, I’m a Frontend
+        Developer at Imperial Overseas, where I’ve built dynamic applications
+        that meet client needs and push the boundaries of usability. I love
+        collaborating with teams to turn creative ideas into functional designs,
+        and I’m always exploring new ways to improve my skills. Let’s connect if
+        you share a passion for web development or have exciting opportunities
+        to discuss!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrap(About, "about");
